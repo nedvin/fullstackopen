7eb1c13fd54a852767a794259d00ca6f9473f53d
nedvin <niemi.edvin@outlook.com>
2023-11-30 11:55:48 +0100

    exercise 4.17

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -1,13 +1,12 @@
 const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
+const User = require("../models/user");
 
 blogsRouter.get("/", async (request, response) => {
-  const blogs = (await Blog.find({})).map((blog) => {
-    if (blog.likes === undefined) {
-      blog.likes = 0;
-    }
-
-    return blog;
+  const blogs = await Blog.find({}).populate("user", {
+    username: 1,
+    name: 1,
+    id: 1,
   });
 
   response.json(blogs);
@@ -22,9 +21,12 @@ blogsRouter.post("/", async (request, response) => {
     return response.status(400).end();
   }
 
-  const blog = new Blog(request.body);
+  const [anyUser] = await User.find({}).limit(1);
+  const blog = new Blog({ ...request.body, user: anyUser._id });
 
   const result = await blog.save();
+  anyUser.blogs = anyUser.blogs.concat(result._id);
+  await anyUser.save();
 
   response.status(201).json(result);
 });
--- part4/models/user.js
+++ part4/models/user.js
@@ -10,6 +10,7 @@ const userSchema = mongoose.Schema({
   },
   name: String,
   passwordHash: String,
+  blogs: [{ type: mongoose.Schema.Types.ObjectId, ref: "Blog" }],
 });
 
 userSchema.plugin(uniqueValidator);
