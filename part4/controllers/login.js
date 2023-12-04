eab74f263ffe9a24c70fc048c040b2ef801bd264
nedvin <niemi.edvin@outlook.com>
2023-12-04 08:47:06 +0100

    bug fix

--- part4/controllers/login.js
+++ part4/controllers/login.js
@@ -8,10 +8,10 @@ loginRouter.post("/", async (request, response) => {
   const { username, password } = request.body;
 
   const user = await User.findOne({ username });
-
+  console.log("user", user);
   const passwordCorrect =
-    user === null ? false : bcrypt.compare(password, user.passwordHash);
-
+    user === null ? false : await bcrypt.compare(password, user.passwordHash);
+  console.log("passwordCorrect", passwordCorrect);
   if (!(user && passwordCorrect)) {
     return response.status(401).json({
       error: "invalid username or password",
